76e7cfb1482eb53271b339eef16bd8d5dc1ff388
Jacob Dye <jacob.dye@atlasschool.com>
2024-12-13 02:41:34 -0600

    Changed the city in weather.js to output a more accurate reading

--- Sale/weather.js
+++ Sale/weather.js
@@ -1,13 +1,15 @@
-const apiKey = '40e589ff13fdbe17bb79ac28e1743f59'; // Replace with your OpenWeatherMap API key
-const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Elgin,AZ,US&units=imperial&appid=${apiKey}`;
+const apiKey = '40e589ff13fdbe17bb79ac28e1743f59';
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Sierra Vista,AZ,US&units=imperial&appid=${apiKey}`;
 
 fetch(apiUrl)
     .then(response => response.json())
     .then(data => {
         const weatherContainer = document.getElementById('weather-container');
+        const temperature = data.main.temp.toFixed(1);
         const weatherHtml = `
+        
             <h2>Weather in ${data.name}</h2>
-            <p>Temperature: ${data.main.temp}°F</p>
+            <p>Temperature: ${temperature}°F</p>
             <p>Weather: ${data.weather[0].description}</p>
             <p>Humidity: ${data.main.humidity}%</p>
             <p>Wind Speed: ${data.wind.speed} mph</p>
